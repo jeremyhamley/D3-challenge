616aa7c91b518b3351ee91a843a698497da95746
Jeremy Hamley <jeremyhamley@gmail.com>
2020-09-09 20:22:26 -0400

    save

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -91,7 +91,7 @@ d3.csv("D3_data_journalism/data/data.csv").then(function(censusData) {
       .attr("class", "tooltip")
       .offset([80, -60])
       .html(function(data, index) {
-        return (`${data.abbr}<br>Poverty: ${data.poverty}<br>Smokes: ${data.smokes}`);
+        return (`${data.abbr}<br>Poverty: ${data.poverty} %<br>Smokers: ${data.smokes} %`);
       });
 
     // Create tooltip in the chart
@@ -124,7 +124,4 @@ d3.csv("D3_data_journalism/data/data.csv").then(function(censusData) {
   }).catch(function(error) {
     console.log(error);
   });
-
-
-
 
